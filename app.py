921e47a71b0bc96bc920302100146e27889e2439
Colin Wagaba <ckwagaba@gmail.com>
2019-05-31 09:08:49 +0300

    add method to create app instance

--- app.py
+++ app.py
@@ -8,20 +8,26 @@ from flask_sqlalchemy import SQLAlchemy
 # initialize sql-alchemy
 db = SQLAlchemy()
 
-# import config options
-from config import app_config
+def create_app():
+    """ creates app instance """
+    # import config options
+    from config import app_config
 
-# get the running config
-config_name = os.getenv('FLASK_APP_CONFIG')
+    # get the running config
+    config_name = os.getenv('FLASK_APP_CONFIG')
 
-app = Flask(__name__)
+    app = Flask(__name__)
 
-# use running config settings on app
-app.config.from_object(app_config[config_name])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # use running config settings on app
+    app.config.from_object(app_config[config_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# add db wrapper
-db.init_app(app)
+    # add db wrapper
+    db.init_app(app)
+
+    return app
+
+app = create_app()
 
 @app.route('/')
 def hello():
--- manage.py
+++ manage.py
@@ -2,22 +2,9 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-# import config options
-from config import app_config
-
 from app import app, db
 
 import models
-
-# get the running config
-config_name = os.getenv('FLASK_APP_CONFIG')
-
-# use running config settings on app
-app.config.from_object(app_config[config_name])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# add db wrapper
-db.init_app(app)
 
 migrate = Migrate(app, db)
 manager = Manager(app)
