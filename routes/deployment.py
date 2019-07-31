19a383afd5658b403855fee96e85ceb47256e5d7
Mubangizi <mubangizia22@gmail.com>
2019-07-31 03:12:28 +0000

    added organisation model

--- routes/deployment.py
+++ routes/deployment.py
@@ -1,6 +1,6 @@
 from os import path
 
-import yaml, base64
+import yaml, base64, logging
 
 from kubernetes import client, config, utils
 
@@ -10,7 +10,7 @@ from flask import request, Blueprint, jsonify
 from helpers.construct_response import *
 
 #kube being our kubernetes instance
-from app import extension_api
+from app import extension_api, kube
 
 deployment_bp = Blueprint('deployment', __name__)
 
@@ -28,48 +28,20 @@ def yamldeployment():
         body=dep_yaml, namespace=namespace, _preload_content=False)
         return "Deployment Successful: "+str(resp.status)
 
-    except:
+    except client.rest.ApiException as e:
+        logging.exception(e)
+        return 'Error Already exits {}'.format(e)
+
     
-        return 'Error Already exits'
-
-    # if (str(resp.status) == '201'):
-    #     response = jsonify({
-    #         msg: 'Deployed'
-    #     })
-    #     response.status_code = resp.status
-    #     return response
-    # elif(str(resp.status) == '409'):
-    #     response = jsonify({
-    #         msg: 'Deployment Exists'
-    #     })
-    #     response.status_code = resp.status
-    #     return response
-    # elif(str(resp.status) == '400'):
-    #     response = jsonify({
-    #         msg: 'Bad data'
-    #     })
-    #     response.status_code = resp.status
-    #     return response
-    
-   
-
-
 #deleting a deployment
 @deployment_bp.route('/deploy/delete/<string:deployment_name>/<string:namespace>',methods = ['POST'])
-def _delete_deployment(deployment_name, namespace):
-    print("Deleting GKE app deployment")
-    extensions_v1beta1 = client.ExtensionsV1beta1Api()
-    delete_options = client.V1DeleteOptions()
-    delete_options.grace_period_seconds = 0
-    delete_options.propagation_policy = 'Foreground'
+def delete_deployment(deployment_name, namespace):
     try:
-        api_response = extensions_v1beta1.delete_namespaced_deployment(
-            name=deployment_name,
-            body=delete_options,
-            grace_period_seconds=2,
-            # namespace="default"
-            namespace = namespace)
-        print("Delete deployment response:%s" % api_response)
-    except Exception as e:
-        print('Error '+str(e))
-        raise e 
+        api_response = extension_api.delete_namespaced_deployment(name=deployment_name, namespace = namespace, async_req=False)
+        print("Deleted deployment response:%s" % api_response)
+        logging.info('deleted deployment /{} from {} namespace'.format(deployment_name, namespace))
+        return 'deleted deployment {} from {} namespace'.format(deployment_name, namespace) 
+    except client.rest.ApiException as e:
+        logging.exception(e)
+        return "Error: {}".format(e)
+   
