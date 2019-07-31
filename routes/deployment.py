c6f09bf89c4fd91f6f071dc3373a0ab20a1d294e
Mubangizi <mubangizia22@gmail.com>
2019-07-31 04:16:04 +0000

    added namespace list retrieval

--- routes/deployment.py
+++ routes/deployment.py
@@ -34,7 +34,7 @@ def yamldeployment():
 
     
 #deleting a deployment
-@deployment_bp.route('/deploy/delete/<string:deployment_name>/<string:namespace>',methods = ['POST'])
+@deployment_bp.route('/deploy/delete/deployment/<string:deployment_name>/<string:namespace>',methods = ['POST'])
 def delete_deployment(deployment_name, namespace):
     try:
         api_response = extension_api.delete_namespaced_deployment(name=deployment_name, namespace = namespace, async_req=False)
@@ -44,6 +44,19 @@ def delete_deployment(deployment_name, namespace):
     except client.rest.ApiException as e:
         logging.exception(e)
         return "Error: {}".format(e)
+
+
+#Getting namespaces
+@deployment_bp.route('/deploy/get/namespaces', methods = ['GET'])
+def get_namespaces():
+        resp = kube.list_namespace()
+        namlist = [i.metadata.name for i in resp.items]
+        response = jsonify({
+            'name': namlist
+        })
+        response.status_code = 200
+        return response
+
    
 
 #Creating namespace
@@ -74,3 +87,38 @@ def delete_namespace(namespace):
         logging.info('Deleted {} namespace'.format(namespace))
         return 'Deleted {} namespace'.format(namespace)
 
+
+
+##TODO: test the following
+
+@deployment_bp.route('/deploy/update/service/<string:deployment_name>/<string:namespace>',methods = ['POST'])
+def update_service(service_object):
+    #upload file
+    encoded_file = request.get_json()['yaml_file']
+    namespace = request.get_json()['namespace']
+    yaml_object = yaml.safe_load(base64.b64decode(encoded_file))
+    name = yaml_object.metadata.name
+    if namespace is None:
+        namespace = service_object.metadata.namespace
+
+    try:
+        service = kube.patch_namespaced_service(name, namespace, yaml_object)
+    except client.rest.ApiException as e:
+        logging.exception(e)
+        return e
+    else:
+        logging.info('updated svc/{} in ns/{}'.format(name, namespace))
+        return 'updated svc/{} from ns/{}'.format(name, namespace)
+
+#deleting a service
+@deployment_bp.route('/deploy/delete/service/<string:service_name>/<string:namespace>', methods = ['POST'])
+def delete_service(service_name, namespace):
+    try:
+        resp = kube.delete_namespaced_service(service_name, namespace)
+        return "Service Deleted: " +str(resp.status)
+    except client.rest.ApiException as e:
+        logging.exception(e)
+        return "Error: {}".format(e)
+    else:
+        logging.info('deleted svc/{} from ns/{}'.format(service_name, namespace))
+        return 'deleted svc/{} from ns/{}'.format(service_name, namespace)
