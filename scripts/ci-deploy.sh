6182e2807e58c6065ba8b82e769fc9ffd283be4e
Colin Wagaba <ckwagaba@gmail.com>
2019-08-21 14:20:15 +0300

    add cert authentication

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 # tag image with hash of commit it was built from
-IMAGE=ckwagaba/crane-cloud-backend:$CIRCLE_SHA1
+IMAGE=ckwagaba/osprey-backend:$CIRCLE_SHA1
 # build image
 docker build -t $IMAGE .
 
@@ -16,10 +16,11 @@ export COMMIT_SHA1=$CIRCLE_SHA1
 envsubst < ./kube/deployment.yml > ./kube/deployment.out.yml
 mv ./kube/deployment.out.yml ./kube/deployment.yml
 
-# echo "$KUBE_CERT" | base64 --decode > cert.crt
+echo "$KUBE_CERT" | base64 --decode > cert.crt
 
 ./kubectl \
   --kubeconfig=/dev/null \
   --server=$KUBE_HOST \
+  --certificate-authority=cert.crt \
   --token=$KUBE_TOKEN \
   apply -f ./kube/
