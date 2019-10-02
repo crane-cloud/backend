6787ff6a45a5f8f2b394c8b9631683ffb8d29c8e
Mubangizi <mubangizia22@gmail.com>
2019-10-02 14:09:49 +0300

    removed unnecessary code

--- routes/organisation.py
+++ routes/organisation.py
@@ -38,11 +38,11 @@ def register_organisation(name):
 # Deleting an Organisation
 @organisation_bp.route('/delete/organisation', methods=['DELETE'])
 def delete_organisation():
-
+    
     org_name = request.get_json()["organisation_name"]
     organisation = Organisation.query.filter_by(name = org_name).first()
 
-    if organisation is not None:
+    if organisation:
         organisation.delete()
         response = jsonify({
             'message': 'Successfully deleted'
@@ -90,18 +90,14 @@ def add_namespace():
 @organisation_bp.route('/organisation/show/namespaces', methods=['GET'])
 def get_organisations_namespaces():
     org_name = request.get_json()["organisation_name"]
-    namespace_name = request.get_json()["namespace"]
-
     organisation = Organisation.query.filter_by(name=org_name).first()
-    namespace = Namespace.query.filter_by(name= namespace_name)
 
-    if organisation and namespace:
-        namespaces = Namespace.query.filter_by(organisation_id=organisation.id).all()
+    if organisation:
         namespace_list = get_all_organisations_namespaces(organisation.id)
         return namespace_list
     else:
         response = jsonify({
-            "message": "Namespace or Organisation does not exist"
+            "message": "Organisation does not exist"
         })
         return response
 
