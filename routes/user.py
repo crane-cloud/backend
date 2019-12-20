3129f1a3d64cbbb4f64ad21980ca71aa25d940c9
Derrick Sekidde <kidricederek@gmail.com>
2019-12-20 12:19:53 +0300

    bug(Mail duplication):Fix email error - Add fix for duplication error when email exists [finishes #4321]

--- routes/user.py
+++ routes/user.py
@@ -31,9 +31,10 @@ def register():
     # validate input
     if str(email).strip() and str(password).strip():
 
+        # check wether this email is in the database
         user_existant = User.query.filter_by(email=email).first()
 
-        if user_existant:
+        if user_existant: # if email exists
             response = jsonify({"message": "Email address already in use."})
             response.status_code = 401
             return response
