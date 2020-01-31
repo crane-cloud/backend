cd406253c131b8284f77b8ad342b73278adc9477
Mubangizi <mubangizia22@gmail.com>
2020-01-31 15:17:56 +0300

    bug-fix(RoleDetailView): proper importation of roleDetailView

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -6,7 +6,7 @@ from .organisation import OrganisationsView, OrganisationDetailView
 from .namespaces import NamespacesView, OrganisationNamespaceView, NamespaceDetailView
 from .deployments import DeploymentsView
 from .clusters import ClustersView, ClusterDetailView
-from .roles import RolesView
+from .roles import RolesView, RolesDetailView
 from .user_role import UserRolesView
 from .organisation_members import OrgMemberView
 from .organisation_admins import OrgAdminView, OrgAdminDetailView
