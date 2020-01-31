865d17ce75ac20f683c032d1d4b608acb9604f56
Mubangizi <mubangizia22@gmail.com>
2020-01-31 15:24:38 +0300

    bug-fix(RolesDetailView): Proper importation of RolesDetailView

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,7 +2,7 @@ from flask_restful import Api
 from app.controllers import (
     IndexView, UsersView, UserLoginView, OrganisationsView,
     OrganisationDetailView, NamespacesView, OrganisationNamespaceView,
-    NamespaceDetailView, DeploymentsView, RolesView, UserRolesView, ClustersView,
+    NamespaceDetailView, DeploymentsView, RolesView, RolesDetailView, UserRolesView, ClustersView,
     OrgMemberView, OrgMemberDetailView, OrgAdminView, ClusterDetailView
 )
 
