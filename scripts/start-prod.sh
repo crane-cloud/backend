d3e20f346494fb49eb07a237de599a1b473bc138
Innocent Asiimwe <innocent.asiimwe@andela.com>
2020-04-07 02:01:55 +0300

    Update prod start script

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
+
 # apply migrations onto db
 python manage.py db upgrade
 
 # start server
-gunicorn --workers=2 --bind 0.0.0.0:5000 server:app
+gunicorn --worker-tmp-dir /dev/shm --workers=4 --bind 0.0.0.0:5000 server:app
