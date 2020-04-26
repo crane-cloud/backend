2d7505d25b4f0d386e9ed3828dc93c5bbb3c5cf5
Innocent Asiimwe <innocent.asiimwe@andela.com>
2020-04-27 01:10:03 +0300

    Add wait for db deployment completion

--- app/controllers/app.py
+++ app/controllers/app.py
@@ -11,6 +11,7 @@ from app.helpers.admin import is_owner_or_admin
 from app.helpers.decorators import admin_required
 from app.helpers.alias import create_alias
 from app.helpers.secret_generator import generate_password, generate_db_uri
+from app.helpers.connectivity import is_database_ready
 
 
 class AppsView(Resource):
@@ -47,13 +48,15 @@ class AppsView(Resource):
             app_alias = create_alias(validated_app_data['name'])
             app_image = validated_app_data['image']
             command = validated_app_data.get('command', None)
-            need_db = validated_app_data.get('need_db', True)
+            need_db = validated_app_data.get('need_db', False)
             project_id = validated_app_data['project_id']
             # env_vars = validated_app_data['env_vars']
             env_vars = validated_app_data.get('env_vars', None)
             project = Project.get_by_id(project_id)
             replicas = 1
             app_port = validated_app_data['port']
+
+            command = command.split() if command else None
 
             if not project:
                 return dict(status='fail', message=f'project {project_id} not found'), 404
@@ -82,7 +85,7 @@ class AppsView(Resource):
             if need_db:
 
                 # create postgres pvc meta and spec
-                pvc_name = f'{app_name}-psql-pvc'
+                pvc_name = f'{app_alias}-psql-pvc'
                 pvc_meta = client.V1ObjectMeta(name=pvc_name)
 
                 access_modes = ['ReadWriteOnce']
@@ -93,7 +96,7 @@ class AppsView(Resource):
                     access_modes=access_modes, resources=resources, storage_class_name=storage_class)
 
                 # create postgres deployment
-                pg_app_name = f'{app_name}-postgres-db'
+                pg_app_name = f'{app_alias}-postgres-db'
 
                 # pg vars
                 POSTGRES_PASSWORD = generate_password(10)
@@ -149,7 +152,7 @@ class AppsView(Resource):
                 )
 
                 pg_service_spec = client.V1ServiceSpec(
-                    type='ClusterIP',
+                    type='NodePort',
                     ports=[client.V1ServicePort(port=5432, target_port=5432)],
                     selector={'app': pg_app_name}
                 )
@@ -165,7 +168,14 @@ class AppsView(Resource):
                     _preload_content=False
                 )
 
-        # hold till pg is ready
+                # get pg_service port
+                pg_service_created = kube.read_namespaced_service(name=pg_app_name, namespace=namespace)
+                pg_service_port = pg_service_created.spec.ports[0].node_port
+
+                # hold here till pg is ready
+                if not is_database_ready(service_host, pg_service_port, 20):
+                    return dict(status='fail', message='Failed at Database creation'), 500
+
 
             # create deployment
             dep_name = f'{app_alias}-deployment'
@@ -191,7 +201,7 @@ class AppsView(Resource):
                 image=app_image,
                 ports=[client.V1ContainerPort(container_port=app_port)],
                 env=env,
-                command=command.split()
+                command=command
             )
 
             # spec
@@ -306,10 +316,14 @@ class ProjectAppsView(Resource):
             app_name = validated_app_data['name']
             app_alias = create_alias(validated_app_data['name'])
             app_image = validated_app_data['image']
+            command = validated_app_data.get('command', None)
+            need_db = validated_app_data.get('need_db', True)
             env_vars = validated_app_data['env_vars']
             project = Project.get_by_id(project_id)
             replicas = 1
             app_port = validated_app_data['port']
+
+            command = command.split() if command else None
 
             if not project:
                 return dict(status='fail', message=f'project {project_id} not found'), 404
@@ -329,16 +343,119 @@ class ProjectAppsView(Resource):
             if app:
                 return dict(status='fail', message=f'app {app_name} already exists'), 409
 
+            kube_host = cluster.host
+            kube_token = cluster.token
+            service_host = urlsplit(kube_host).hostname
+
+            kube, extension_api, appsv1_api, api_client, batchv1_api, storageV1Api = create_kube_clients(kube_host, kube_token)
 
             # create the app
             new_app = App(name=app_name, image=app_image, project_id=project_id, alias=app_alias, port=app_port)
- 
+
+            if need_db:
+
+                # create postgres pvc meta and spec
+                pvc_name = f'{app_alias}-psql-pvc'
+                pvc_meta = client.V1ObjectMeta(name=pvc_name)
+
+                access_modes = ['ReadWriteOnce']
+                storage_class = 'openebs-standard'
+                resources = client.V1ResourceRequirements(requests=dict(storage='1Gi'))
+
+                pvc_spec = client.V1PersistentVolumeClaimSpec(
+                    access_modes=access_modes, resources=resources, storage_class_name=storage_class)
+
+                # create postgres deployment
+                pg_app_name = f'{app_alias}-postgres-db'
+
+                # pg vars
+                POSTGRES_PASSWORD = generate_password(10)
+                POSTGRES_USER = app_name
+                POSTGRES_DB = app_name
+
+                DATABASE_URI = generate_db_uri(pg_app_name, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB)
+
+                pg_env = [
+                    client.V1EnvVar(name='POSTGRES_PASSWORD', value=POSTGRES_PASSWORD),
+                    client.V1EnvVar(name='POSTGRES_USER', value=POSTGRES_USER),
+                    client.V1EnvVar(name='POSTGRES_DB', value=POSTGRES_DB)
+                ]
+                pg_container = client.V1Container(
+                    name=pg_app_name,
+                    image='postgres:10.8-alpine',
+                    ports=[client.V1ContainerPort(container_port=5432)],
+                    env=pg_env
+                )
+
+                pg_template = client.V1PodTemplateSpec(
+                    metadata=client.V1ObjectMeta(labels={
+                        'app': pg_app_name
+                    }),
+                    spec=client.V1PodSpec(containers=[pg_container])
+
+                )
+
+                pg_spec = client.V1DeploymentSpec(
+                    replicas=1,
+                    template=pg_template,
+                    selector={'matchLabels': {'app': pg_app_name}}
+                )
+
+                pg_deployment = client.V1Deployment(
+                    api_version="apps/v1",
+                    kind="Deployment",
+                    metadata=client.V1ObjectMeta(name=pg_app_name),
+                    spec=pg_spec
+                )
+
+                # postgres deployment
+                appsv1_api.create_namespaced_deployment(
+                    body=pg_deployment,
+                    namespace=namespace,
+                    _preload_content=False
+                )
+
+                # postgres service
+                pg_service_meta = client.V1ObjectMeta(
+                    name=pg_app_name,
+                    labels={'app': pg_app_name}
+                )
+
+                pg_service_spec = client.V1ServiceSpec(
+                    type='NodePort',
+                    ports=[client.V1ServicePort(port=5432, target_port=5432)],
+                    selector={'app': pg_app_name}
+                )
+
+                pg_service = client.V1Service(
+                    metadata=pg_service_meta,
+                    spec=pg_service_spec
+                )
+
+                kube.create_namespaced_service(
+                    namespace=namespace,
+                    body=pg_service,
+                    _preload_content=False
+                )
+
+                # get pg_service port
+                pg_service_created = kube.read_namespaced_service(name=pg_app_name, namespace=namespace)
+                pg_service_port = pg_service_created.spec.ports[0].node_port
+
+                # hold here till pg is ready
+                if not is_database_ready(service_host, pg_service_port, 20):
+                    return dict(status='fail', message='Failed at Database creation'), 500
 
             # create deployment
             dep_name = f'{app_alias}-deployment'
 
             # EnvVar
             env = []
+
+            if DATABASE_URI:
+                env.append(client.V1EnvVar(
+                    name='DATABASE_URI', value=DATABASE_URI
+                ))
 
             if env_vars:
                 for key, value in env_vars.items():
@@ -351,8 +468,9 @@ class ProjectAppsView(Resource):
                 name=app_alias,
                 image=app_image,
                 ports=[client.V1ContainerPort(container_port=app_port)],
-                env=env
-            )
+                env=env,
+                command=command
+                )
 
             # spec
             template = client.V1PodTemplateSpec(
@@ -378,11 +496,6 @@ class ProjectAppsView(Resource):
             )
 
             # create deployment in  cluster
-            kube_host = cluster.host
-            kube_token = cluster.token
-            service_host = urlsplit(kube_host).hostname
-
-            kube, extension_api, appsv1_api, api_client, batchv1_api, storageV1Api = create_kube_clients(kube_host, kube_token)
 
             appsv1_api.create_namespaced_deployment(
                 body=deployment,
@@ -416,6 +529,7 @@ class ProjectAppsView(Resource):
 
             service = kube.read_namespaced_service(name=service_name, namespace=namespace)
             service_port = service.spec.ports[0].node_port
+
 
             service_url = f'http://{service_host}:{service_port}'
 
