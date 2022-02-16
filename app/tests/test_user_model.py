a602080bca140f52febd98fdbe893ca16da489a3
e-ian <ianemma70@gmail.com>
2022-02-16 15:55:10 +0300

    add database fixture, functional tests

--- app/tests/test_user_model.py
+++
@@ -1,6 +0,0 @@
-def test_create_user_with_fixture(new_user):
-    assert new_user.email == 'test_email@testdomain.com'
-    assert new_user.password != 'test_password'
-    assert new_user.name == 'test_name'
-
-
