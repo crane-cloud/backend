312fd213c1f3ab6987c71423ed67530c680a8d1c
e-ian <ianemma70@gmail.com>
2022-03-25 12:44:08 +0300

    - add billing metrics model

---
+++ app/models/billing_metrics.py
@@ -0,0 +1,21 @@
+from sqlalchemy.dialects.postgresql import UUID
+from ..models import db
+from sqlalchemy import text as sa_text
+
+from app.models.model_mixin import ModelMixin
+
+
+class BillingMetrics(ModelMixin):
+    """ billing metrics table definition """
+
+    __tablename__ = 'billing_metrics'
+
+    # billing metrics fields
+    id = db.Column(UUID(as_uuid=True), primary_key=True, 
+                    server_default=sa_text("uuid_generate_v4()"))
+    app_id = db.Column(UUID(as_uuid=True), db.ForeignKey('app.id'), nullable=False)
+    invoice_id = db.relationship('BillingInvoice', backref='owner', lazy=True)
+    memory = db.Column(db.Integer, nullable=True)
+    cpu = db.Column(db.Integer, nullable=True)
+    network = db.Column(db.Integer, nullable=True)
+    storage = db.Column(db.Integer, nullable=True)
