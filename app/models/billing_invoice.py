09becc4e9adf0f9c54c99b451ee53349130987fb
e-ian <ianemma70@gmail.com>
2022-03-25 12:43:49 +0300

    - add billing invoice model

---
+++ app/models/billing_invoice.py
@@ -0,0 +1,20 @@
+from sqlalchemy.dialects.postgresql import UUID
+from ..models import db
+from sqlalchemy import text as sa_text
+
+from app.models.model_mixin import ModelMixin
+
+
+class BillingInvoice(ModelMixin):
+    """ billing invoice table definition"""
+
+    __tablename__ = 'billing_invoices'
+
+    # billing invoice fields
+    id = db.Column(UUID(as_uuid=True), primary_key=True, 
+                    server_default=sa_text("uuid_generate_v4()"))
+    total_amount = db.Column(db.Integer, nullable=True, default=0.00)
+    date_cashed = db.Column(db.Datetime)
+    is_cashed = db.Column(db.Boolean, nullable=False, default=False)
+    project_id = db.Column(UUID(as_uuid=True), db.ForeignKey('project.id'), nullable=False)
+    
