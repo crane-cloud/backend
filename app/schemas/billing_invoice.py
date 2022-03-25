ac501511ded59a73a8342375e9cc125327374f89
e-ian <ianemma70@gmail.com>
2022-03-25 12:44:45 +0300

    - billing invoice schema

---
+++ app/schemas/billing_invoice.py
@@ -0,0 +1,23 @@
+from marshmallow import Schema, fields, validate
+from app.helpers.age_utility import get_item_age
+
+
+class BillingInvoiceSchema(Schema):
+
+    id = fields.String(dump_only=True)
+
+    project_id = fields.String(required=True, error_message={
+        "required": "project_id is required"},
+        validate=[
+            validate.Regexp(
+                regex=r'^(?!\s*$)', error='project_id should be a valid string'
+            ),
+    ])
+
+    total_amount = fields.Int()
+    date_cashed = fields.Date(dump_only=True)
+    is_cashed = fields.Boolean()
+    age = fields.Method("get_age", dump_only=True)
+
+    def get_age(self, obj):
+        return get_item_age(obj.date_cashed)
