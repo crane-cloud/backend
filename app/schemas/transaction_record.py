2c243c83e52557dab284e8d199e8796dda9dae90
e-ian <ianemma70@gmail.com>
2022-04-13 19:40:47 +0300

    add transaction record schema

---
+++ app/schemas/transaction_record.py
@@ -0,0 +1,28 @@
+from marshmallow import Schema, fields, validate
+from app.helpers.age_utility import get_item_age
+
+
+class TransactionRecordSchema(Schema):
+
+    id = fields.UUID(dump_only=True)
+    owner_id = fields.UUID(required=True, error_message={
+        "required": "owner_id is required"
+    })
+    amount = fields.Int()
+    currency = fields.String()
+    name = fields.String(required=False, error_message={
+        "required": "name is required"},
+        validate=[
+            validate.Regexp(
+                regex=r'^(?!\s*$)', error='name should be a valid string'
+            ),
+    ])
+    email = fields.Email(required=False)
+    phone_number = fields.String()
+    flutterwave_ref = fields.String()
+    status = fields.String()
+    tx_ref = fields.String()
+    transaction_id = fields.Int()
+
+    def get_age(self, obj):
+        return get_item_age(obj.date_created)
