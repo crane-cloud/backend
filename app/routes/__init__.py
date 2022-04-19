d827c190be1a75fa420891bbcdc7f9bce846a559
e-ian <ianemma70@gmail.com>
2022-04-19 13:40:50 +0300

    - add route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -16,6 +16,7 @@ from app.controllers import (
     ProjectDatabaseResetView, ProjectDatabaseAdminResetView, ProjectDatabasePasswordResetView, ProjectDatabaseAdminPasswordResetView,
     ProjectDatabaseRetrievePasswordView, ProjectDatabaseAdminRetrievePasswordView, DatabaseStatsView, AppDataSummaryView,
     UserAdminUpdateView, AppRevertView, TransactionRecordView)
+from app.controllers.transactions import TransactionRecordDetailView
 
 
 api = Api()
@@ -84,6 +85,7 @@ api.add_resource(UserRolesView, '/user/<string:user_id>/roles',
 
 # Transaction routes
 api.add_resource(TransactionRecordView, '/transactions', endpoint='transactions')
+api.add_resource(TransactionRecordDetailView, '/transactions/<string:record_id>')
 
 # Project route
 api.add_resource(ProjectsView, '/projects', endpoint='projects')
