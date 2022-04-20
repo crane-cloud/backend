c332af34400b53a64b6ebebd48664d0f62da0e4a
e-ian <ianemma70@gmail.com>
2022-04-20 21:06:53 +0300

    - extend model

--- app/models/transaction_record.py
+++ app/models/transaction_record.py
@@ -14,10 +14,11 @@ class TransactionRecord(ModelMixin):
     # transaction record fields
     id = db.Column(UUID(as_uuid=True), primary_key=True, server_default=sa_text("uuid_generate_v4()"))
     owner_id = db.Column(UUID(as_uuid=True), db.ForeignKey('user.id'), nullable=False)
+    project_id = db.Column(UUID(as_uuid=True), db.ForeignKey('project.id'), nullable=False)
     amount = db.Column(db.Integer, nullable=True, default=0.00)
     currency = db.Column(db.String(256), nullable=True)
     name = db.Column(db.String(256), nullable=True)
-    email = db.Column(db.String(256), unique=True, nullable=True, default="")
+    email = db.Column(db.String(256), unique=False, nullable=True, default="")
     phone_number = db.Column(db.String(256), nullable=True, default="")
     flutterwave_ref = db.Column(db.String(256), nullable=True)
     status = db.Column(db.String(256), nullable=True)
