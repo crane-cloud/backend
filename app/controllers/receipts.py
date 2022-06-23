891ebaacc98bee2f80d5b2a535094daa751a41ec
e-ian <ianemma70@gmail.com>
2022-06-23 22:13:16 +0300

    - receipts detail api

---
+++ app/controllers/receipts.py
@@ -0,0 +1,82 @@
+
+
+import json
+import pdb
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt_claims
+from app.helpers.admin import is_owner_or_admin
+from app.models.project import Project
+from app.models.transaction_record import TransactionRecord
+from app.schemas.billing_receipts import BillingReceiptSchema
+
+
+class BillingReceiptsView(Resource):
+    """
+    - Billing receipts view
+    """
+
+    @jwt_required
+    def get(self, project_id):
+
+        current_user_id = get_jwt_identity()
+        current_user_roles = get_jwt_claims()['roles']
+
+        billing_receipt_schema = BillingReceiptSchema(many=True)
+        project = Project.get_by_id(project_id)
+
+        if not project:
+            return dict(status='fail', message='Project with project id {project_id} not found'), 404
+
+        
+        if not is_owner_or_admin(project, current_user_id, current_user_roles):
+                return dict(status='fail', message='Unauthorised'), 403
+
+        billing_receipts = TransactionRecord.find_all(project_id=project_id)
+
+        if not billing_receipts:
+            return dict(
+                status='fail',
+                message=f'billing receipt records not found'
+            ), 404
+
+        billing_receipts_data, errors = billing_receipt_schema.dumps(billing_receipts)
+
+        if errors:
+            return dict(status='fail', message=errors), 500
+
+        return dict(status='success', data=dict(
+            billing_receipts=json.loads(billing_receipts_data))), 200
+
+
+class BillingReceiptsDetailView(Resource):
+
+    @jwt_required
+    def get(self, project_id ,receipt_id):
+
+        current_user_id = get_jwt_identity()
+        current_user_roles = get_jwt_claims()['roles']
+
+        billing_invoice_schema = BillingReceiptSchema()
+        project = Project.get_by_id(project_id)
+
+        if not project:
+            return dict(status='fail', message='Project with project id not found'), 404
+
+        if not is_owner_or_admin(project, current_user_id, current_user_roles):
+                return dict(status='fail', message='Unauthorised'), 403
+
+        billing_receipt = TransactionRecord.get_by_id(receipt_id)
+
+        if not billing_receipt:
+            return dict(
+                status='fail',
+                message=f'billing receipt with invoice id {receipt_id} not found'
+            ), 404
+
+        billing_receipt_data, errors = billing_invoice_schema.dumps(billing_receipt)
+        
+        if errors:
+            return dict(status='fail', message=errors), 500
+
+        return dict(status='success', data=dict(
+            billing_receipt=json.loads(billing_receipt_data))), 200
