e04998f570662f3bf3c75c06496608309665171d
e-ian <ianemma70@gmail.com>
2022-10-10 18:23:10 +0300

    refactor purchase, promotion credit names

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -153,7 +153,7 @@ def sendExpirationNotification():
                 template = "user/credit_expiration_notification.html"
                 subject = "Promotional Credits Expiration from Crane Cloud Project"
                 email =user.email
-                amount = user_credit.amount_promotion_credits
+                amount = user_credit.promotion_credits
                 today = notification_datetime
                 success = False
 
