d101e5f41a81ef8213fe739537d719fc78d87c9c
bashaashma <bashaashma@gmail.com>
2022-10-29 10:44:55 -0400

    change secret key

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -17,7 +17,7 @@ def new_user(test_client):
 @pytest.fixture(scope='function')
 def test_client():
     flask_app = create_app(config_name='testing')
-    flask_app.config['SECRET_KEY'] = 'sekrit!'
+    flask_app.config['SECRET_KEY'] = '192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
     # create a test client using the Flask application configured for testing
     with flask_app.test_client() as testing_client:
         # Establish an application context
