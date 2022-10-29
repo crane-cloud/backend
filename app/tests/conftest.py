f9c4d69f40f36df730bec66aac2cf5f5e04c9195
bashaashma <bashaashma@gmail.com>
2022-10-29 09:21:59 -0400

    "chnage conftest"

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 
 from app.models.user import User
 from server import create_app, db
-import os
+
 
 @pytest.fixture(scope='function')
 def new_user(test_client):
@@ -41,7 +41,3 @@ def login_default_user(test_client):
                      )
 
     yield  # testing happens
-
-@pytest.fixture(scope="function")
-def configured_env(monkeypatch, test_client):
-    monkeypatch.setenv("JWT_SECRET_KEY", os.getenv("FLASK_APP_SECRET"))
