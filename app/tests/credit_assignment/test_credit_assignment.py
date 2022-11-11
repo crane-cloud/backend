bf7dab2a0826c761959e3f5badd3ca6a39b926ee
bashaashma <bashaashma@gmail.com>
2022-11-11 09:01:03 -0500

    added more tests

--- app/tests/credit_assignment/test_credit_assignment.py
+++ app/tests/credit_assignment/test_credit_assignment.py
@@ -138,3 +138,76 @@ def test_credits_assignment_invalid_token(test_client, admin_login_user, login_u
         json=assignment_data,)
     
     assert response.status_code == 422
+
+
+def test_credits_assignment_detail(test_client, admin_login_user, login_user):
+
+    """
+    GIVEN  user and admin creation when user and admin already exist and users have credits
+    WHEN the users have been assigned credits '/credit_assignment/{user_id}' page is requested (GET)
+    THEN check that the response is valid
+    """
+    #assign credits
+    assignment_data = {'amount':100, 'description': 'test', 'user_id':'{}'.format(login_user.user.id)}
+    response = test_client.post(
+        '/credit_assignment',
+        content_type ='application/json',
+        headers = admin_login_user.headers,
+        json=assignment_data,)
+
+    #get credits
+    response = test_client.get(
+        '/credit_assignment/{}'.format(login_user.user.id),
+        content_type ='application/json',
+        headers = login_user.headers)
+
+    assert response.status_code == 200
+
+
+def test_credits_assignment_detail_invalid_token(test_client, admin_login_user, login_user):
+
+    """
+    GIVEN  user and admin creation when user and admin already exist and users have credits
+    WHEN the users have been assigned credits '/credit_assignment/{user_id}' page is requested (GET) with invalid token
+    THEN check that the response is valid
+    """
+    #assign credits
+    assignment_data = {'amount':100, 'description': 'test', 'user_id':'{}'.format(login_user.user.id)}
+    response = test_client.post(
+        '/credit_assignment',
+        content_type ='application/json',
+        headers = admin_login_user.headers,
+        json=assignment_data,)
+
+    #get credits
+    header = {'Authorization': 'Bearer 1111111111'}
+    response = test_client.get(
+        '/credit_assignment/{}'.format(login_user.user.id),
+        content_type ='application/json',
+        headers = header)
+
+    assert response.status_code == 422
+
+def test_credits_assignment_detail_unauthorized_user(test_client, admin_login_user, login_user):
+
+    """
+    GIVEN  user and admin creation when user and admin already exist and users have credits
+    WHEN the users have been assigned credits '/credit_assignment/{user_id}' page is requested (GET) with invalid token
+    THEN check that the response is valid
+    """
+    #assign credits
+    assignment_data = {'amount':100, 'description': 'test', 'user_id':'{}'.format(login_user.user.id)}
+    response = test_client.post(
+        '/credit_assignment',
+        content_type ='application/json',
+        headers = admin_login_user.headers,
+        json=assignment_data,)
+
+    #get credits
+    non_existing_user = {'1111111111'}
+    response = test_client.get(
+        '/credit_assignment/{}'.format(non_existing_user),
+        content_type ='application/json',
+        headers = admin_login_user.headers)
+
+    assert response.status_code == 404
