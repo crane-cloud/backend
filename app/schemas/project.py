b266dbef2d40dcec8dc64ab4ad41f5e6822fb7cc
bashaashma <bashaashma@gmail.com>
2023-03-29 10:15:32 -0400

    show project and database disabled for UI

--- app/schemas/project.py
+++ app/schemas/project.py
@@ -25,6 +25,8 @@ class ProjectSchema(Schema):
     date_created = fields.Date(dump_only=True)
     age = fields.Method("get_age", dump_only=True)
     apps_count = fields.Method("get_apps_count", dump_only=True)
+    disabled = fields.Boolean()
+
 
     def get_age(self, obj):
         return get_item_age(obj.date_created)
