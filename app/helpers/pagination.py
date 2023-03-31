f13c12af3da769a9b2c135c059c5d91d653de463
LanternNassi <71936382+LanternNassi@users.noreply.github.com>
2023-03-31 23:32:07 +0300

    Adding number of pages to the pagination information returned (#404)

--- app/helpers/pagination.py
+++ app/helpers/pagination.py
@@ -1,4 +1,4 @@
-
+import math
 
 def paginate(items,per_page,page):
 
@@ -8,9 +8,10 @@ def paginate(items,per_page,page):
         'per_page' : per_page,
         'count' : len(paginated_items),
         'total_count' : len(items),
+        'pages' : math.ceil(len(items)/per_page) ,
         'page' : page,
         'next' : page+1 if per_page*page < len(items) else 'null',
-        'previous' : 'null' if page ==1 else page-1
+        'previous' : 'null' if page ==1 else page-1 
     }
 
     return pagination , paginated_items
