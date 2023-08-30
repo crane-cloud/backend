c6935a6d6d736dbd14a4c1e4a1cf9404809435fb
LanternNassi <Lanternnassi@gmail.com>
2023-08-30 16:20:42 +0300

    Adding the overall system endpoint

--- app/controllers/system_status.py
+++ app/controllers/system_status.py
@@ -5,6 +5,11 @@ from sqlalchemy import null
 from app.controllers.statuses import get_client_status_infor, get_cluster_status_info, get_database_status_infor, get_prometheus_status_info
 from app.models.clusters import Cluster
 from flask import request
+from app.helpers.decorators import admin_required
+from app.models.user import User
+from app.models.project import Project
+from app.models.app import App
+from app.models.project_database import ProjectDatabase
 
 
 class SystemStatusView(Resource):
@@ -44,3 +49,47 @@ class SystemStatusView(Resource):
             'database_status': database_status,
             'mira_status': mira_status
         }), 200
+
+
+class SystemSummaryView(Resource):
+
+    @admin_required
+    def get(self):
+
+        # Users 
+        user_count = User.query.count()
+        verified_users = User.query.filter(User.verified==True).count()
+        beta_users = User.query.filter(User.is_beta_user==True).count()
+
+        #Projects
+        project_count = Project.query.count()
+        active_projects = Project.query.filter(Project.disabled == False).count()
+
+        #apps
+        app_count = App.query.count()
+
+
+        #Databases
+        database_count = ProjectDatabase.query.count()
+        mysql_database_count = ProjectDatabase.query.filter(ProjectDatabase.database_flavour_name == 'mysql').count()
+
+
+        return dict(status='success' , data={
+            'Users' : {
+                'total_count' : user_count,
+                'verified' : verified_users,
+                'beta_users' : beta_users
+            },
+            'Projects' : {
+                'total_count' : project_count,
+                'active' : active_projects
+            },
+            'Apps' : {
+                'total_count' : app_count,
+            },
+            'Databases' : {
+                'total_count' : database_count,
+                'mysql' : mysql_database_count,
+                'postgres' : database_count - mysql_database_count 
+            }
+        }) , 200
