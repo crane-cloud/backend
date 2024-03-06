48ded8f58bae5f40128479ea447e024b22368819
khalifan-kfan <khalifanmuwonge@gmail.com>
2024-03-06 18:37:50 +0300

    chore: enable addition of logs to the service

--- app/helpers/activity_logger.py
+++ app/helpers/activity_logger.py
@@ -5,8 +5,14 @@ from app.models.user import User
 from app.tasks import celery_app
 from flask_pymongo import MongoClient
 import os
+import requests
+import json
+from app.helpers.crane_app_logger import logger
+
 mongo = MongoClient(
     os.getenv('MONGO_URI', 'mongodb://localhost:27017/cranecloud'))
+
+LOGGER_APP_URL = os.getenv('LOGGING_SERVICE_URL', None)
 
 try:
     mongo_db = mongo.get_default_database()
@@ -15,28 +21,39 @@ except Exception as e:
 
 
 def log_activity(model: str, status: str, operation: str, description: str, a_user_id=None, a_db_id=None, a_app_id=None, a_project_id=None, a_cluster_id=None):
-    user_id = get_jwt_identity()
-    user = User.get_by_id(user_id)
-    user_email = user.email if user else None
-    user_name = user.name if user else None
-    date = str(datetime.datetime.now())
-    data = {
-        'user_id': user_id,
-        'user_email': user_email,
-        'user_name': user_name,
-        'creation_date': date,
-        'operation': operation,
-        'model': model,
-        'status': status,
-        'description': description,
-        'a_user_id': str(a_user_id) if a_user_id else None,
-        'a_db_id': str(a_db_id) if a_db_id else None,
-        'a_app_id': str(a_app_id) if a_app_id else None,
-        'a_project_id': str(a_project_id) if a_project_id else None,
-        'a_cluster_id': str(a_cluster_id) if a_cluster_id else None
-    }
+    if not LOGGER_APP_URL:
+        return
+    try:
+        user_id = get_jwt_identity()
+        user = User.get_by_id(user_id)
+        user_email = user.email if user else None
+        user_name = user.name if user else None
+        date = str(datetime.datetime.now())
+        data = {
+            'user_id': user_id,
+            'user_email': user_email,
+            'user_name': user_name,
+            'creation_date': date,
+            'operation': operation,
+            'model': model,
+            'status': status,
+            'description': description,
+            'a_user_id': str(a_user_id) if a_user_id else None,
+            'a_db_id': str(a_db_id) if a_db_id else None,
+            'a_app_id': str(a_app_id) if a_app_id else None,
+            'a_project_id': str(a_project_id) if a_project_id else None,
+            'a_cluster_id': str(a_cluster_id) if a_cluster_id else None
+        }
 
-    log_user_activity.delay(data)
+        result = requests.post(
+            f"{LOGGER_APP_URL}/api/activities", json=data)
+        log = result.json()
+        logger.info(f"Logging activity: {log['message']}")
+    except Exception as e:
+        logger.error(f"Error logging activity")
+        pass
+
+    # log_user_activity.delay(data)
 
 
 @celery_app.task(name="log_user_activity")
