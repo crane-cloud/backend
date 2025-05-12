798e6e5c9eff85be4df848ce2c0163294d8f253c
khalifan-kfan <khalifanmuwonge@gmail.com>
2025-05-12 11:53:39 +0300

    fix email link for forgot password and verification

--- app/helpers/confirmation.py
+++ app/helpers/confirmation.py
@@ -6,6 +6,6 @@ from .token import generate_token
 def send_verification(email, name, url, secret_key, password_salt, sender, app, template, subject):
     token = generate_token(email, secret_key, password_salt)
     verify_url = url + token
-    html = render_template(template, verify_url=verify_url, email=email, name=name)
+    html = render_template(template, frontend_url=verify_url, email=email, name=name)
     subject = subject
     send_email(email, subject, html, sender, app)
