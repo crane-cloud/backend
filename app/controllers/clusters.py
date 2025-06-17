4e3b29f0e66491429a1dcda01b9ba665b62a036d
khalifan-kfan <khalifanmuwonge@gmail.com>
2025-06-17 14:51:03 +0300

    format code

--- app/controllers/clusters.py
+++ app/controllers/clusters.py
@@ -146,14 +146,7 @@ class ClusterDetailView(Resource):
         except client.rest.ApiException as e:
             error_message = "Could not access cluster or invalid token, please reset your cluster token." \
             if e.status == 401 or "Unauthorized" in str(e) else str(e)
-            return dict(
-            status='fail',
-            data=dict(
-                cluster=json.loads(validated_cluster_data),
-                resource_count=[]
-            ),
-            cluster_error=error_message
-            ), 200
+            return dict(status='fail', data=dict( cluster=json.loads(validated_cluster_data), resource_count=[]),cluster_error=error_message), 200
 
         except Exception as e:
             return dict(status='fail', message=str(e)), 500
