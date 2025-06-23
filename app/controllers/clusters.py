0537476697faf7e7c280d7d64f90ab9ee8bb11f9
khalifan-kfan <khalifanmuwonge@gmail.com>
2025-06-23 16:02:53 +0300

    fix return cluster nodes list

--- app/controllers/clusters.py
+++ app/controllers/clusters.py
@@ -298,8 +298,9 @@ class ClusterNodesView(Resource):
 
             # get all nodes in the cluster
             node_resp = kube_client.kube.list_node()
+            print(node_resp)
 
-            pagination, paginated_items = paginate(node_resp, per_page, page)
+            pagination, paginated_items = paginate(node_resp.items, per_page, page)
 
             nodes = []
 
